84c9ed582b265ab1a9ac4ed417d167a7ba41393f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-01-31 18:20:53 +0100

    added data

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+    name: 'AppTitle',
+    data: () => ({
+        text: 'La mia prima app con Vite!'
+    })
+};
+</script>
+
+<template></template>
+
+
+<style></style>
